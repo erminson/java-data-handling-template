ec812cc57f3ba12fb5167f7e4d0a7cc174e60f7e
Lev Erminson <erminson@gmail.com>
2021-02-15 17:59:48 +0300

    Implemented method `toJumpCase` in class SimpleTextService

--- src/main/java/com/epam/izh/rd/online/service/SimpleTextService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleTextService.java
@@ -1,16 +1,17 @@
 package com.epam.izh.rd.online.service;
 
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 public class SimpleTextService implements TextService {
 
     /**
      * Реализовать функционал удаления строки из другой строки.
-     *
+     * <p>
      * Например для базовой строки "Hello, hello, hello, how low?" и строки для удаления ", he"
      * метод вернет "Hellollollo, how low?"
      *
-     * @param base - базовая строка с текстом
+     * @param base   - базовая строка с текстом
      * @param remove - строка которую необходимо удалить
      */
     @Override
@@ -20,7 +21,7 @@ public class SimpleTextService implements TextService {
 
     /**
      * Реализовать функционал проверки на то, что строка заканчивается знаком вопроса.
-     *
+     * <p>
      * Например для строки "Hello, hello, hello, how low?" метод вернет true
      * Например для строки "Hello, hello, hello!" метод вернет false
      */
@@ -31,7 +32,7 @@ public class SimpleTextService implements TextService {
 
     /**
      * Реализовать функционал соединения переданных строк.
-     *
+     * <p>
      * Например для параметров {"Smells", " ", "Like", " ", "Teen", " ", "Spirit"}
      * метод вернет "Smells Like Teen Spirit"
      */
@@ -43,24 +44,32 @@ public class SimpleTextService implements TextService {
     /**
      * Реализовать функционал изменения регистра в вид лесенки.
      * Возвращаемый текст должен начинаться с прописного регистра.
-     *
+     * <p>
      * Например для строки "Load Up On Guns And Bring Your Friends"
      * метод вернет "lOaD Up oN GuNs aNd bRiNg yOuR FrIeNdS".
      */
     @Override
     public String toJumpCase(String text) {
-        return null; //TODO
+        return IntStream
+                .range(0, text.length())
+                .map(i -> i % 2 == 0
+                        ? Character.toLowerCase(text.charAt(i))
+                        : Character.toUpperCase(text.charAt(i)))
+                .collect(StringBuilder::new,
+                        StringBuilder::appendCodePoint,
+                        StringBuilder::append)
+                .toString();
     }
 
     /**
      * Метод определяет, является ли строка палиндромом.
-     *
+     * <p>
      * Палиндром - строка, которая одинаково читается слева направо и справа налево.
-     *
+     * <p>
      * Например для строки "а роза упала на лапу Азора" вернется true, а для "я не палиндром" false
      */
     @Override
     public boolean isPalindrome(String string) {
-       return false; //TODO
+        return false; //TODO
     }
 }
